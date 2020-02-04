529b00f92f8ce3ed6cfab27701e0b94954106235
mrphuca9pro <thienphuc.esv@gmail.com>
2020-02-04 22:02:30 +0700

    2 files modified, 7 added

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Yasuo', desc: 'Mid'},
+  {id: 12, name: 'Yasuo', desc: 'Mid'},
+  {id: 13, name: 'Yasuo', desc: 'Mid'},
+  {id: 14, name: 'Yasuo', desc: 'Mid'},
+  {id: 15, name: 'Yasuo', desc: 'Mid'},
+  {id: 16, name: 'Yasuo', desc: 'Mid'},
+  {id: 17, name: 'Yasuo', desc: 'Mid'},
+  {id: 18, name: 'Yasuo', desc: 'Mid'},
+  {id: 19, name: 'Yasuo', desc: 'Mid'},
+  {id: 20, name: 'Kalista', desc: 'Bottom'},
+];
